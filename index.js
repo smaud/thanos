3212824d872ce8497d18d50a348267286901b4be
SM <54699996+smaud@users.noreply.github.com>
2021-03-04 12:10:46 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-const testFolder = './.ssh/';
+const testFolder = '~/.ssh/';
 const fs = require('fs');
 
 fs.readdir(testFolder, (err, files) => {
-   console.log(`your ssh key directory contains:`);
+  console.log(`your ssh key directory contains:`);
   files.forEach(file => {
     console.log(file);
   });
